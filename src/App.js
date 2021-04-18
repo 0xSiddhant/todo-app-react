682c1f78e0abc41c03b5ed2347c69581e79c402f
Siddhant2155 <contactsiddhant2155@gmail.com>
2021-04-19 01:45:37 +0530

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Siddhant";
   return (
     <div className="App">
       <header className="App-header">
+        {20 + 30}
+        <h1>{name}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
